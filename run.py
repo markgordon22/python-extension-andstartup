20484c215ca141291f3f8d65492dc8d77bf088e9
markgordon22 <markgordon97@gmail.com>
2021-05-11 10:55:03 +0000

    hhhh

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 import json
 from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
-    import env
+  import env
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -30,7 +30,8 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return render_template("member.html", member=member)
+    return render_template("member.html", m=member)
+    
 
 
 @app.route("/contact", methods = ["GET", "POST"])
